5069378bbc73f0d034d06de63527eaf1b9860225
Roderick Smith <68719939+rickygeetchi@users.noreply.github.com>
2022-04-06 20:15:32 -0500

    Update main.js
    addng semicolons

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ const hasLicense = await input.confirm("Do you have your license? :");
 const userAge = Number(await input.text("How old are you?"));
 
 if (hasLicense && userAge >= usDrivingAge) {
-    console.log("You can drive!")
-} else if (hasLicense && userAge < usDrivingAge){
-    console.log("We're talking about a US driver's license, right? How'd you get one?")
+    console.log("You can drive!");
+} else if (hasLicense && userAge < usDrivingAge) {
+    console.log("We're talking about a US driver's license, right? How'd you get one?");
 }
 else {
-    console.log("Sorry, you cannont drive.")
+    console.log("Sorry, you cannont drive.");
 }
