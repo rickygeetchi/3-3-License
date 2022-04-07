a734ecf16c8f023b5772a8f006a2a3792295b61b
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-04-06 20:13:08 -0500

    Solutions for the second assignment

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import input from 'input';
+
+const usDrivingAge = 16;
+const hasLicense = await input.confirm("Do you have your license? :");
+
+
+const userAge = Number(await input.text("How old are you?"));
+
+if (hasLicense && userAge >= usDrivingAge) {
+    console.log("You can drive!")
+} else if (hasLicense && userAge < usDrivingAge){
+    console.log("We're talking about a US driver's license, right? How'd you get one?")
+}
+else {
+    console.log("Sorry, you cannont drive.")
+}
